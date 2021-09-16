6cfe680a8561f3e18da9460a989e45502fc2d81d
Pao-Hua Chien <200453641@student.georgianc.on.ca>
2021-09-16 14:32:06 -0700

    Add project files.

---
+++ HotHitsLyrics/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HotHitsLyrics.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

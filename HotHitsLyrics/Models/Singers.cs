afeb4a3099d62381b12eb1b0744d31d83ca413d5
Pao-Hua Chien <200453641@student.georgianc.on.ca>
2021-09-25 21:03:06 -0700

    Created 2 Models Singers and Songs

---
+++ HotHitsLyrics/Models/Singers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotHitsLyrics.Models
+{
+    public class Singers
+    {
+        //Create Primary Key and related fields of Singers
+        public int SingersId { get; set; }
+        public string Name { get; set; }
+        public string Language { get; set; }
+        public string Country { get; set; }
+    }
+}
---
+++ HotHitsLyrics/Models/Songs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotHitsLyrics.Models
+{
+    public class Songs
+    {
+        //Create Primary Key, Foreign Key and related fields of Songs
+        public int SongsId { get; set; }
+        public string SongName { get; set; }
+        public string Genre { get; set; }
+        public int ReleasedYear { get; set; }
+        public int SingersId { get; set; }
+    }
+}

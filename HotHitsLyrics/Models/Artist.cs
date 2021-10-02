683016b6d13d76a24abebdf264db3164d34627da
Pao-Hua Chien <200453641@student.georgianc.on.ca>
2021-10-01 18:12:56 -0700

    Deleted old models, recreated Artist model Deleted the SongsController

---
+++ HotHitsLyrics/Models/Artist.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotHitsLyrics.Models
+{
+    public class Artist
+    {
+        //Primary Key
+        public int ArtistId { get; set; }
+
+        //Only represent Date
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+
+        [MaxLength(100)]
+        public string Nationality { get; set; }
+
+    }
+}

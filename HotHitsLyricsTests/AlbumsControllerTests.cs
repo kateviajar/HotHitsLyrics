354ef4c277281f521e1479a58d459a3ad7ef4ab2
Pao-Hua Chien <200453641@student.georgianc.on.ca>
2021-11-24 15:07:30 -0800

    Create a Test project

---
+++ HotHitsLyricsTests/AlbumsControllerTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HotHitsLyricsTests
+{
+    [TestClass]
+    public class AlbumsControllerTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
